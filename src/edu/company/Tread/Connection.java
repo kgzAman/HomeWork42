404afabdcb76541e1674b4f57dd12649d37acc90
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-03-24 16:51:09 +0600

    #6 make sendToAll method

--- src/edu/company/Tread/Connection.java
+++ src/edu/company/Tread/Connection.java
@@ -4,76 +4,84 @@ import Client.EchoClient;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.*;
 
-public class Connection extends Thread{
+public class Connection implements Runnable {
     private final Socket clientSocked;
-    public ArrayList<EchoClient> list=new ArrayList<>();
-
+    private static ArrayList<EchoClient> list = new ArrayList<>();
 
 
     public Connection(Socket clientSocked) {
         this.clientSocked = clientSocked;
     }
 
+    public static void handle(Socket socket) {
+        String username = "Client" + socket.getPort();
+        System.out.println("Подключен клиент:" + socket);
+        try (
+                socket;
+                Scanner reader = Connection.getReader(socket);
+                PrintWriter writer = getWriter(socket);) {
+            Connection.sendResponse("Hello " + username, writer);
 
-    @Override
-    public void run() {
-            System.out.println("Подключен клиент:"+clientSocked);
-            try (
-                    clientSocked;
-                    Scanner reader = getReader(clientSocked);
-                    PrintWriter writer = getWriter(clientSocked);){
-                sendResponse("Hello "+ clientSocked,writer);
-
-                while (true){
-                    String message = reader.nextLine();
-                    if(isEmptyMsg(message)|| isQuitMsg(message)){
-                        break;
-                    }
-                    sendResponse(message.toUpperCase(),writer);
+            while (true) {
+                String message = reader.nextLine();
+                sendToAll(username, socket, message);
+                if (Connection.isEmptyMsg(message) || Connection.isQuitMsg(message)) {
+                    break;
                 }
-            }catch (NoSuchElementException ex ){
-                System.out.println("Клиент закрыл соединение! ");
-            }catch (IOException e ){
-                System.out.printf("Клиент отключен: %s%n",clientSocked);
+//                CsendToAll(username,clientSocked,message);
+//                Connection.sendResponse(message.toUpperCase(), writer);
             }
+        } catch (NoSuchElementException ex) {
+            System.out.println("Клиент закрыл соединение! ");
+        } catch (IOException e) {
+            System.out.printf("Клиент отключен: %s%n", socket);
         }
 
-    private PrintWriter getWriter(Socket socket)throws IOException {
-        OutputStream stream = socket.getOutputStream();
-        return  new PrintWriter(socket.getOutputStream());
     }
 
-    private Scanner getReader(Socket socket)throws  IOException{
+
+    public static PrintWriter getWriter(Socket socket) throws IOException {
+        OutputStream stream = socket.getOutputStream();
+        return new PrintWriter(stream);
+    }
+
+    public static Scanner getReader(Socket socket) throws IOException {
         InputStream stream = socket.getInputStream();
         InputStreamReader input = new InputStreamReader(stream, StandardCharsets.UTF_8);
-        return  new Scanner(input);
+        return new Scanner(input);
     }
 
-    private boolean isQuitMsg(String message){
+    public static boolean isQuitMsg(String message) {
         return "bye".equals(message.toLowerCase());
     }
 
-    private   boolean isEmptyMsg(String message){
-        return message==null || message.isBlank();
+    public static boolean isEmptyMsg(String message) {
+        return message == null || message.isBlank();
     }
 
-    private void sendResponse(String response,Writer writer)throws IOException{
+    public static void sendResponse(String response, Writer writer) throws IOException {
         writer.write(response);
         writer.write(System.lineSeparator());
         writer.flush();
     }
 
-    public static void sendToAll(String name, Socket socket, String message){
-        .stream().filter(c -> c.getSocket() != socket).forEach(c -> {
+    public static void sendToAll(String name, Socket socket, String message) {
+        list.stream().filter(c -> c.getSocket() != socket).forEach(c -> {
             try {
-                PrintWriter printWriter = Action.getWriter(c.getSocket());
-                Action.sendResponse(name + ": " + message, printWriter);
+                PrintWriter printWriter = getWriter(c.getSocket());
+                sendResponse(name + ": " + message, printWriter);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
+    }
+
+    @Override
+    public void run() {
+        list.add(new EchoClient(clientSocked));
+        handle(clientSocked);
+
+    }
 }
--- src/edu/company/Tread/EchoServer.java
+++ src/edu/company/Tread/EchoServer.java
@@ -1,34 +1,39 @@
 package edu.company.Tread;
-import javax.swing.*;
+import Client.EchoClient;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class EchoServer{
-        private final int port;
-        private final ExecutorService pool = Executors.newCachedThreadPool();
+public class EchoServer {
+    private final int port;
+    private final ExecutorService pool = Executors.newCachedThreadPool();
 
 
-        EchoServer(int port) {
-            this.port = port;
-        }
 
-        public void  run(){
-            try (ServerSocket server = new ServerSocket(port)){
-             while (!server.isClosed()){
-                 Socket clientSocked = server.accept();
-                     pool.submit(new Connection(clientSocked));
-
-
-             }
-            } catch (IOException e) {
-                var formatMsg = "Port is busy!";
-                System.out.printf(formatMsg,port);
-                e.printStackTrace();
-            }
-        }
-
+    EchoServer(int port) {
+        this.port = port;
     }
+
+    public void run() {
+        try (ServerSocket server = new ServerSocket(port)) {
+            while (!server.isClosed()) {
+                Socket clientSocked = server.accept();
+                pool.submit(new Connection(clientSocked));
+
+
+            }
+        } catch (IOException e) {
+            var formatMsg = "Port is busy!";
+            System.out.printf(formatMsg, port);
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+}
